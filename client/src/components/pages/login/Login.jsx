cafc02e2641d38c108ef5851ca75880b7813f47c
amandena <manda.m.johnson@gmail.com>
2022-07-22 13:36:10 -0400

    created login component and styles

---
+++ client/src/components/pages/login/Login.jsx
@@ -0,0 +1,33 @@
+import "./login.scss"
+
+const Login = () => {
+
+  return (
+    <div className="login">
+      <div className="top">
+        <div className="wrapper">
+          <img 
+            className="logo" 
+            src="https://upload.wikimedia.org/wikipedia/commons/6/67/NewNetflixLogo.png" 
+            alt="" 
+            />
+        </div>
+      </div>
+      <div className="container">
+        <form>
+          <h1>Sign In</h1>
+          <input type="email" placeholder="Email or phone number" />
+          <input type="password" placeholder="Password" />
+          <button className="loginButton">Sign In</button>
+          <span>New to Netflix? <b>Sign up now.</b></span>
+          <small>
+            This page is protected by Google reCAPTCHA to ensure you're not a
+            bot. <b>Learn more</b>.
+          </small>
+        </form>
+      </div>
+    </div>
+  )
+}
+
+export default Login
