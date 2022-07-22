4b6395aa5f30849b53feff65d413140d70cf06b4
amandena <manda.m.johnson@gmail.com>
2022-07-21 22:38:19 -0400

    imported list component

--- client/src/home/Home.jsx
+++ client/src/home/Home.jsx
@@ -1,11 +1,16 @@
 import "./home.scss"
 import Navbar from "../components/navbar/Navbar"
+import Featured from "../components/featured/Featured"
+import List from "../components/list/List"
 
 const Home = () => {
   return (
     <div className="home">
       <Navbar />
-      <img style={{width: "100%"}} src="https://upload.wikimedia.org/wikipedia/commons/c/c6/Pizza_%2848496390846%29.jpg" alt="" />
+      <Featured type="movie" />
+      <List />
+      <List />
+      <List />
     </div>
   )
 }
