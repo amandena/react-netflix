3229b75d11ec1b4378554ac9952aa9cf5c245a50
amandena <manda.m.johnson@gmail.com>
2022-08-01 13:54:27 -0400

    copy-pasted code to find errors

--- api/index.js
+++ api/index.js
@@ -2,7 +2,8 @@ const express = require("express")
 const app = express()
 const mongoose = require("mongoose")
 const dotenv = require("dotenv")
-const authRoute = require("./routes/auth.js")
+const authRoute = require("./routes/auth")
+const userRoute = require("./routes/users")
 
 dotenv.config()
 
@@ -15,7 +16,7 @@ mongoose.connect(process.env.MONGO_URL, {
 app.use(express.json())
 
 app.use("/api/auth", authRoute)
-
+app.use("/api/users", userRoute)
 
 app.listen(8800, () => {
   console.log("Backend server is running!")
