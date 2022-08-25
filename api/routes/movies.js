6d985d5bee50aa51c2927d9001d6f9d38098a3be
amandena <manda.m.johnson@gmail.com>
2022-08-24 23:05:33 -0400

    added get random route to movies

--- api/routes/movies.js
+++ api/routes/movies.js
@@ -63,5 +63,27 @@ router.get("/:id", verify, async (req, res) => {
   }
 });
 
+// GET RANDOM
+router.get("/random", verify, async (req, res) => {
+  const type = req.query.type
+  let movie
+  try {
+    if (type === "series") {
+      movie = await Movie.aggregate([
+        { $match: { isSeries: true } },
+        { $sample: { size: 1 } },
+      ])
+    } else {
+      movie = await Movie.aggregate([
+        { $match: { isSeries: false } },
+        { $sample: { size: 1 } },
+      ])
+    }
+    res.status(200).json(movie)
+  } catch (err) {
+    res.status(500).json(err)
+  }
+});
+
 
 module.exports = router
