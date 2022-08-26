2acac753d7d74c26533229e8fbe6374a43b4d159
amandena <manda.m.johnson@gmail.com>
2022-08-25 21:21:23 -0400

    fixed movie model

--- api/models/Movie.js
+++ api/models/Movie.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose")
 
 const MovieSchema = new mongoose.Schema({
   title: { type: String, required: true, unique: true },
-  desc: { type: Stringe },
+  desc: { type: String },
   img: { type: String },
   imgTitle: { type: String },
   imgSm: { type: String },
